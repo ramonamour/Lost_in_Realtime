19ad457f8d8f4c2aa5896f11fcf29085dca283c0
ramonamour <44301743+ramonamour@users.noreply.github.com>
2023-06-15 15:30:13 +0200

    update

---
+++ Lost In Realtime/Assets/Scripts/FlickeringEmissive.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class FlickeringEmissive : MonoBehaviour
+{
+    [SerializeField] private bool Flicker;
+    [SerializeField] [Min(0)] private float FlickerSpeed = 1f;
+
+    [SerializeField] AnimationCurve BrightnessCurve;
+
+    private Renderer Renderer;
+    private List<Material> Materials = new();
+    private List<Color> InitialColors = new();
+
+    private const string EMISSIVE_COLOR_NAME = "_EmissionColor";
+    private const string EMISSIVE_KEYWORD = "_EMISSION";
+    // Start is called before the first frame update
+    void Awake()
+    {
+        Renderer = GetComponent<Renderer>();
+        BrightnessCurve.postWrapMode = WrapMode.Loop;
+
+        foreach(Material material in Renderer.materials) {
+            if ( Renderer.material.HasColor(EMISSIVE_COLOR_NAME))
+            {
+                Materials.Add(material);
+                InitialColors.Add(material.GetColor(EMISSIVE_COLOR_NAME));
+            }
+            else
+            {
+                Debug.LogWarning($"{material.name} material not emissive");
+            }
+        }
+
+        if (Materials.Count == 0)
+        {
+            enabled = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Flicker && Renderer.isVisible) {
+            float scaledTime = Time.time * FlickerSpeed;
+
+            for (int i = 0; i < Materials.Count; i++)
+            {
+                Color color = InitialColors[i];
+                float brightness = BrightnessCurve.Evaluate(scaledTime);
+                color = new Color(
+                    color.r * Mathf.Pow(2, brightness),
+                    color.g * Mathf.Pow(2, brightness),
+                    color.b * Mathf.Pow(2, brightness),
+                    color.a
+                );
+                Materials[i].SetColor(EMISSIVE_COLOR_NAME, color);
+            }
+        }
+        
+    }
+}
